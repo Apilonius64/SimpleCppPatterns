e0d19aa1da3faf1e5e8e26c636ce1f22c4688aff
Apilonius64 <HenryFell@outlook.de>
2023-07-02 18:59:16 +0200

    Better test code

--- main.cpp
+++ main.cpp
@@ -3,11 +3,19 @@
 int main(int argc, char const *argv[])
 {
     Factory<Anything, AnythingA, AnythingB> fac;
-    ChildFactory<Factory<Anything, AnythingA, AnythingB>, Something, SomethingA, SomethingB> cFac;
+    ChildFactory<
+        Factory<Anything,   AnythingA,  AnythingB>,
+                Something,  SomethingA, SomethingB
+    > cFac;
     Factory<Anything, AnythingA, AnythingB>* cFacPtr = &cFac;
 
     cFacPtr->MakeType(TypeContainer<Anything>())->Print();
     cFacPtr->MakeType(TypeContainer<AnythingA>())->Print();
     cFacPtr->MakeType(TypeContainer<AnythingB>())->Print();
+
+    cFacPtr->Make<Anything>()->Print();
+    cFacPtr->Make<AnythingA>()->Print();
+    cFacPtr->Make<AnythingB>()->Print();
+
     return 0;
 }
