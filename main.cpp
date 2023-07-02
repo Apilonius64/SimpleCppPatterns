d800a762759baade259afe8ef555146cafc8feb9
Apilonius64 <HenryFell@outlook.de>
2023-07-02 18:52:53 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Factory/Factory.h"
+
+int main(int argc, char const *argv[])
+{
+    Factory<Anything, AnythingA, AnythingB> fac;
+    ChildFactory<Factory<Anything, AnythingA, AnythingB>, Something, SomethingA, SomethingB> cFac;
+    Factory<Anything, AnythingA, AnythingB>* cFacPtr = &cFac;
+
+    cFacPtr->MakeType(TypeContainer<Anything>())->Print();
+    cFacPtr->MakeType(TypeContainer<AnythingA>())->Print();
+    cFacPtr->MakeType(TypeContainer<AnythingB>())->Print();
+    return 0;
+}
